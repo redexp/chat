26795b98ab50631ab8ab689b1e802a86c0583f88
Sergii.Kliuchnyk <sergii.kliuchnyk@masterofcode.com>
2016-11-03 08:17:12 +0200

    new animation

--- js/controller/chat.js
+++ js/controller/chat.js
@@ -22,7 +22,7 @@ define('controller/chat', [
     });
 
     dispatcher.on('add-chat-message', function (chat, text) {
-        if (/^\s*новая\s+тема/i.test(text)) {
+        if (/^\s*(новая\s+тема|new)/i.test(text)) {
             chat = addChat();
         }
 
@@ -34,15 +34,40 @@ define('controller/chat', [
     function addChat(data) {
         data = data || {};
 
-        var chat = chatList.add(data);
-        $(this).css('display', 'none');
-        var view = chatList.view.list.get(chat);
-        view.$el
-            .css({'border-left-width': 1})
-            .velocity({'margin-left': 30}, {complete: function () {
-                view.ui.textarea.focus();
-            }})
-        ;
+        var width = chatList.view.$el.width(),
+            last = chatList.view.list.get(chatList.last()),
+            w = last.$el.width(),
+            newW = (width - 30 * chatList.length) / (chatList.length + 1);
+
+        last.$el.css({
+            'flex-grow': 0,
+            'width': w / 2
+        });
+
+        var chat = chatList.add(data),
+            view = chatList.view.list.get(chat);
+
+        view.$el.css({
+            'flex-grow': 0,
+            'width': w / 2,
+            'border-left-width': 1
+        });
+
+        last.$el.velocity({'width': newW}, {complete: function () {
+            last.$el.css({
+                'flex-grow': 1,
+                'width': ''
+            });
+        }});
+
+        view.$el.velocity({'margin-left': 30, 'width': newW}, {complete: function () {
+            view.$el.css({
+                'flex-grow': 1,
+                'width': ''
+            });
+        }});
+
+        view.ui.textarea.focus();
 
         return chat;
     }
